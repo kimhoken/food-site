5a71fdd3803875270f615e9e41456caba29dd9d9
kimhoken <kimhk441@naver.com>
2026-07-06 17:47:17 +0900

    마이페이지 코드 최적화  및 정렬 구현중

---
+++ src/main/java/com/project/foodsite/dto/MypageDTO.java
@@ -0,0 +1,17 @@
+package com.project.foodsite.dto;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Alias("mypage")
+public class MypageDTO extends SearchDTO {
+    private int member_id;
+}
